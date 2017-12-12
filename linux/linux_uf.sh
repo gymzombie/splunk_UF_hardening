361a911a72d1f39862876b23dd96c13b8857e5e4
gymzombie <gymzombie@users.noreply.github.com>
2017-12-12 14:30:30 -0500

    Update linux_uf.sh
    Script now adds output, moved some variables, and creates splunk user account.

--- linux/linux_uf.sh
+++ linux/linux_uf.sh
@@ -1,31 +1,55 @@
 #!/bin/bash
 
+INSTALL_FILE="/home/andy/splunkforwarder-7.0.1-2b5b15c4ee89-Linux-x86_64.tgz"
+
+DEPLOY_SERVER="servername:8089"
+PASSWORD=$(cat /dev/urandom | env LC_CTYPE=C tr -dc a-zA-Z0-9 | head -c 60)
+INSTALL_LOCATION="/opt"
+
 # check for root
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
+  else echo "[!] Thx for running as root. Continuing..."
 fi
 
-# install splunk, potentially needs customized depending on your environment
-yum -y install splunkforwarder
+# unpack tarball to install location
+echo "[*] Unpacking Tarball"
+sudo tar -xzf $INSTALL_FILE -C $INSTALL_LOCATION
 
-# enable boot-start, set to run as user splunk
-/opt/splunkforwarder/bin/splunk enable boot-start -user splunk --accept-license --answer-yes --no-prompt
-
-# disable management port
-mkdir -p /opt/splunkforwarder/etc/apps/UF-TA-killrest/local
-echo '[httpServer]
-disableDefaultPort = true' > /opt/splunkforwarder/etc/apps/UF-TA-killrest/local/server.conf
 
 # ensure splunk home is owned by splunk, except for splunk-launch.conf
-chown -R splunk:splunk /opt/splunkforwarder
-chown root:splunk /opt/splunkforwarder/etc/splunk-launch.conf
-chmod 644 /opt/splunkforwarder/etc/splunk-launch.conf
+echo "[*] Creating splunk user for service account"
+sudo useradd -m -r splunk
+
+echo "[*] Changing ownership of Splunk Forwarder folders"
+chown -R splunk:splunk $INSTALL_LOCATION/splunkforwarder
+chown root:splunk $INSTALL_LOCATION/splunkforwarder/etc/splunk-launch.conf
+chmod 644 $INSTALL_LOCATION/splunkforwarder/etc/splunk-launch.conf
+
+echo "[*] Doing initial run of Splunk install"
+sudo -u splunk $INSTALL_LOCATION/splunkforwarder/bin/splunk start --accept-license --answer-yes --auto-ports --no-prompt
+
+# enable boot-start, set to run as user splunk
+echo "[*] Configuring Splunk Forwarder to start on bootup"
+$INSTALL_LOCATION/splunkforwarder/bin/splunk enable boot-start -user splunk --accept-license --answer-yes --no-prompt
+
+# disable management port
+echo "[*] Disabling management port for security."
+mkdir -p $INSTALL_LOCATION/splunkforwarder/etc/apps/UF-TA-killrest/local
+echo '[httpServer]
+disableDefaultPort = true' > $INSTALL_LOCATION/splunkforwarder/etc/apps/UF-TA-killrest/local/server.conf
+
+# Point to the Deployment server for remote administration
+echo "[*] Configuring Splunk Forwarder for remote administration"
+sudo -u splunk $INSTALL_LOCATION/splunkforwarder/bin/splunk set deploy-poll \"$DEPLOY_SERVER\" --accept-license --answer-yes --auto-ports --no-prompt  -auth admin:changeme
 
 # change admin pass
-/opt/splunkforwarder/bin/splunk edit user admin -password `head -c 500 /dev/urandom | sha256sum | base64 | head -c 16 ; echo` -auth admin:changeme
+echo "[*] Changing administrative credentials"
+$INSTALL_LOCATION/splunkforwarder/bin/splunk edit user admin -password $PASSWORD -auth admin:changeme
 
 # ensure user splunk can read /var/log
+echo "[*] Adding splunk account to read /var/log"
 setfacl -Rm u:splunk:r-x,d:u:splunk:r-x /var/log
 
 # do the same for the audit log
@@ -33,3 +57,8 @@ sed -i 's/log_group = root/log_group = splunk/g' /etc/audit/auditd.conf
 chgrp -R splunk /var/log/audit
 chmod 0750 /var/log/audit
 chmod 0640 /var/log/audit/*
+
+echo "[*] Restarting Splunk to finalize configuration"
+sudo -u splunk $INSTALL_LOCATION/splunkforwarder/bin/splunk restart
+
+echo "[!] Please check for errors, as this install script has limited error checking!. Otherwise, work complete."
